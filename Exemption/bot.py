548ed70f25486d259565319ae065a7e82508a4be
dblancSonrai <86306197+dblancSonrai@users.noreply.github.com>
2024-03-27 10:27:04 -0400

    Update bot.py

--- Exemption/bot.py
+++ Exemption/bot.py
@@ -1,4 +1,5 @@
 import logging
+import json
 from sonrai import gql_loader
 
 def run(ctx):
@@ -8,19 +9,24 @@ def run(ctx):
     # Create GraphQL client
     graphql_client = ctx.graphql_client()
 
-    search_name = ""
+    # Get the ticket data from the context
+    ticket = ctx.config.get('data').get('ticket')
+
+    get_resources = None
+    query_resourcesToExempt = gql['savedQuery.gql']
+    mutation_setImportance = gql['setImportance.gql']
 
     for customField in ticket.get('customFields'):
-        search_name = customField['name']
-
-    query_resourcesToExempt = gql['savedQuery.gql']
-    get_resources = graphql_client.query(query_resourcesToExempt,search_name)
+        search_name = customField['value']
+        search_name = ('{"name": "' + search_name + '" }')
+        get_resources = graphql_client.query(query_resourcesToExempt,search_name)
 
     for resource in get_resources['ExecuteSavedQuery']['Query']['Resources']['items']:
         srn = resource['srn']
-        mutation_setImportance = gql['setImportance.gql']
+        srn = ('{"srn": "' + srn + '" }')
+        print(srn)
         set_importance = graphql_client.query(mutation_setImportance,srn)
         endResource = set_importance['setImportance']['srn']
         logging.info('Exempted Resource: '+endResource)
 
-gql_loader.snooze_ticket(ctx, hours=24)
+#gql_loader.snooze_ticket(ctx, hours=24)
