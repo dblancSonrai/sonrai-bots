d4e3f7cc871eeb6067d4692384a6906324b6cb32
dblancSonrai <86306197+dblancSonrai@users.noreply.github.com>
2024-04-24 12:37:14 -0400

    Update bot.py

--- Exemption/bot.py
+++ Exemption/bot.py
@@ -10,29 +10,28 @@ def run(ctx):
     graphql_client = ctx.graphql_client()
 
     # Get the ticket data from the context
-    #ticket = ctx.config.get('data').get('ticket')
     ticket = ctx.config
     ticketSrn = ticket['data']['ticket']['srn']
-    
+    ticketSrn = ('{"srn": "' + ticketSrn + '" }')
+
+    #Set query/mutation variables
     get_resources = None
     query_resourcesToExempt = gql['savedQuery.gql']
     mutation_setImportance = gql['setImportance.gql']
     query_ticket = gql['ticket.gql']
-    logging.info(ticketSrn)
-    ticketSrn = ('{"srn": "' + ticketSrn + '" }')
-    
-    customField = graphql_client.query(query_ticket,ticketSrn)
-    
-    logging.info(customField)
-    logging.info('Attempting to grab search name')
-    search_name = customField['ListFindings']['items'][0]['cfFields'][0]['value']
-    logging.info('Formatting query name')
-    search_name = ('{"name": "' + search_name + '" }')
-    logging.info('Attempting to run query')
-    get_resources = graphql_client.query(query_resourcesToExempt,search_name)
-    logging.info('Query successfully worked')
 
-    for resource in get_resources['ExecuteSavedQuery']['Query']['Roles']['items']: #change rolls to resources
+    #Ticket look up to get custom fields
+    customField = graphql_client.query(query_ticket,ticketSrn)
+
+    #Format search name for custom search query
+    search_name = customField['ListFindings']['items'][0]['cfFields'][0]['value']
+    search_name = ('{"name": "' + search_name + '" }')
+
+    #Run custom search query to get resources to exempt
+    get_resources = graphql_client.query(query_resourcesToExempt,search_name)
+
+    #exempt the resources
+    for resource in get_resources['ExecuteSavedQuery']['Query']['Resources']['items']: #change rolls to resources
         srn = resource['srn']
         srn = ('{"srn": "' + srn + '" }')
         set_importance = graphql_client.query(mutation_setImportance,srn)
