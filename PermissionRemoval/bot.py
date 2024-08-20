459b2e2b6d2207bc352a5fd38abf27f5c5a3da91
dblancSonrai <86306197+dblancSonrai@users.noreply.github.com>
2024-08-20 10:48:38 -0400

    Update bot.py

--- PermissionRemoval/bot.py
+++ PermissionRemoval/bot.py
@@ -13,7 +13,7 @@ def run(ctx):
     SonraiClient = ctx.get_client()
     logging.info('Client Created')
     logging.info(SonraiClient.credential)
-    logging.info(SonraiClient)
+    logging.info(SonraiClient.get(client_class))
 
     #get ticket date
     ticket = ctx.config
