5af72ece8814d67122ceed3a9a1e2b6dec2c82c3
dblancSonrai <86306197+dblancSonrai@users.noreply.github.com>
2024-08-21 17:08:06 -0400

    Update bot.py

--- PermissionRemoval/bot.py
+++ PermissionRemoval/bot.py
@@ -41,4 +41,4 @@ def run(ctx):
     except Exception as error:
         logging.info('Permission Removal Bot Failed: ')
         logging.info(error)
-        #gql_loader.add_ticket_comment(ctx, "this is a comment")
+        gql_loader.add_ticket_comment(ctx, "this is a comment")
